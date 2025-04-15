564ea0cb3afeb3d43e2ebd78ec9247b8d8041517
Alexa Zhang <xxzhang17@gmail.com>
2025-04-14 21:48:56 -0700

    Update text on topbar to button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,18 +5,18 @@ import './App.css';
 function App() {
   return (
     <div>
-      <div className="top-bar">
+      <div className="top-bar"> // create a component folder that has a separate css and js for each class
         <div className="left">
           <AiFillHome className="home-icon" />
           <span>Welcome to the United Nations</span>
         </div>
         <div className="right">
-          <span>العربية</span>
-          <span>中文</span>
-          <span className="bold">English</span>
-          <span>Français</span>
-          <span>Русский</span>
-          <span>Español</span>
+          <button>العربية</button>
+          <button>中文</button>
+          <button className="bold">English</button>
+          <button>Français</button>
+          <button>Русский</button>
+          <button>Español</button>
         </div>
       </div>
       <div className="blue-bar"></div>
