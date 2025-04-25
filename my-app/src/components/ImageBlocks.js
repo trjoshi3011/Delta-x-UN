99d60161810651b55a7147880b4296e78f3b814a
Alexa Zhang <xxzhang17@gmail.com>
2025-04-25 15:01:50 -0700

    Fix hover

--- my-app/src/components/ImageBlocks.js
+++ my-app/src/components/ImageBlocks.js
@@ -5,6 +5,8 @@ import image2 from '../assets/image2.jpg';
 import image3 from '../assets/image3.jpg';
 import image4 from '../assets/image4.jpg';
 import image5 from '../assets/image5.jpg';
+import image6 from '../assets/image6.jpg';
+import image7 from '../assets/image7.jpg';
 
 function ImageBlocks() {
   return (
@@ -14,15 +16,15 @@ function ImageBlocks() {
         <div className="arrow-line">→</div>
       </div>
 
-      <a className="block news-block" href="https://www.un.org/en/news-1" target="_blank" rel="noopener noreferrer">
-        <img src={image1} alt="News 1" />
+      <a className="block news-block" href="https://storyteller.iom.int/stories/what-war-couldnt-take-stories-courage-and-survival-sudans-displaced" target="_blank" rel="noopener noreferrer">
+        <img src={image6} alt="News 1" />
       </a>
 
-      <a className="block news-block" href="https://www.un.org/en/news-2" target="_blank" rel="noopener noreferrer">
-        <img src={image2} alt="News 2" />
+      <a className="block news-block" href="https://www.unwomen.org/en/articles/explainer/how-gender-inequality-and-climate-change-are-interconnected" target="_blank" rel="noopener noreferrer">
+        <img src={image7} alt="News 2" />
       </a>
 
-      <a className="block news-block" href="https://www.un.org/en/news-3" target="_blank" rel="noopener noreferrer">
+      <a className="block news-block" href="https://www.wfp.org/stories/nourishing-syrias-future-how-wfp-rethinking-nutrition-generation-born-conflict" target="_blank" rel="noopener noreferrer">
         <img src={image4} alt="News 3" />
       </a>
     </div>
