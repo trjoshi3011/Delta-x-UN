4cf85ce2fcdb38c6ec69c0fe82bbfb52b2b5cdb9
Alexa Zhang <xxzhang17@gmail.com>
2025-04-25 15:13:33 -0700

    Scrollable image

--- my-app/src/components/ImageBlocks.js
+++ my-app/src/components/ImageBlocks.js
@@ -15,18 +15,31 @@ function ImageBlocks() {
         <h3>Today<br />at the UN</h3>
         <div className="arrow-line">→</div>
       </div>
+      <div className="scroll-wrapper">
+        <div className="scroll-track">
+          <a className="block news-block" href="https://storyteller.iom.int/stories/what-war-couldnt-take-stories-courage-and-survival-sudans-displaced" target="_blank" rel="noopener noreferrer">
+            <img src={image6} alt="News 1" />
+          </a>
+          <a className="block news-block" href="https://www.unwomen.org/en/articles/explainer/how-gender-inequality-and-climate-change-are-interconnected" target="_blank" rel="noopener noreferrer">
+            <img src={image7} alt="News 2" />
+          </a>
+          <a className="block news-block" href="https://www.wfp.org/stories/nourishing-syrias-future-how-wfp-rethinking-nutrition-generation-born-conflict" target="_blank" rel="noopener noreferrer">
+            <img src={image4} alt="News 3" />
+          </a>
 
-      <a className="block news-block" href="https://storyteller.iom.int/stories/what-war-couldnt-take-stories-courage-and-survival-sudans-displaced" target="_blank" rel="noopener noreferrer">
-        <img src={image6} alt="News 1" />
-      </a>
+          <a className="block news-block" href="https://www.wfp.org/stories/nourishing-syrias-future-how-wfp-rethinking-nutrition-generation-born-conflict" target="_blank" rel="noopener noreferrer">
+            <img src={image2} alt="News 4" />
+          </a>
 
-      <a className="block news-block" href="https://www.unwomen.org/en/articles/explainer/how-gender-inequality-and-climate-change-are-interconnected" target="_blank" rel="noopener noreferrer">
-        <img src={image7} alt="News 2" />
-      </a>
+          <a className="block news-block" href="https://www.wfp.org/stories/nourishing-syrias-future-how-wfp-rethinking-nutrition-generation-born-conflict" target="_blank" rel="noopener noreferrer">
+            <img src={image1} alt="News 5" />
+          </a>
+        </div>
+      </div>
+      
 
-      <a className="block news-block" href="https://www.wfp.org/stories/nourishing-syrias-future-how-wfp-rethinking-nutrition-generation-born-conflict" target="_blank" rel="noopener noreferrer">
-        <img src={image4} alt="News 3" />
-      </a>
+
+      
     </div>
   );
 }
