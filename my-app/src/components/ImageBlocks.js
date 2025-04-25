18c87f2fd07646cb1c970f8df78206e565ffc027
Alexa Zhang <xxzhang17@gmail.com>
2025-04-25 14:55:09 -0700

    Auto zoom the images and hover magnify

--- my-app/src/components/ImageBlocks.js
+++ my-app/src/components/ImageBlocks.js
@@ -3,25 +3,27 @@ import './ImageBlocks.css';
 import image1 from '../assets/image1.jpg';
 import image2 from '../assets/image2.jpg';
 import image3 from '../assets/image3.jpg';
+import image4 from '../assets/image4.jpg';
+import image5 from '../assets/image5.jpg';
 
 function ImageBlocks() {
   return (
     <div className="image-blocks">
       <div className="block today-un">
-        <h3>Today at the UN</h3>
+        <h3>Today<br />at the UN</h3>
+        <div className="arrow-line">→</div>
       </div>
-      <a className="block" href="#">
+
+      <a className="block news-block" href="https://www.un.org/en/news-1" target="_blank" rel="noopener noreferrer">
         <img src={image1} alt="News 1" />
-        <h4>THE COMMITTEES: General Assembly’s Sixth Committee…</h4>
-        <p>28 December 2018 | Global</p>
       </a>
-      <a className="block" href="#">
+
+      <a className="block news-block" href="https://www.un.org/en/news-2" target="_blank" rel="noopener noreferrer">
         <img src={image2} alt="News 2" />
-        <h4>Worth going ‘extra mile’ for a new Syrian constitution…</h4>
       </a>
-      <a className="block" href="#">
-        <img src={image3} alt="News 3" />
-        <h4><span className="highlight">Humanitarian Aid</span> | DR Congo: Record numbers face acute…</h4>
+
+      <a className="block news-block" href="https://www.un.org/en/news-3" target="_blank" rel="noopener noreferrer">
+        <img src={image4} alt="News 3" />
       </a>
     </div>
   );
