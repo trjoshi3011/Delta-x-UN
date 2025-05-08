166c75e8277f2fca4abe09aceda2d1adebc298d6
patelpriya0507 <150724615+patelpriya0507@users.noreply.github.com>
2025-05-07 20:12:59 -0700

    completed

--- my-app/src/components/UNPhoto/UNPhotoImage.js
+++ my-app/src/components/UNPhoto/UNPhotoImage.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './UNPhotoImage.css';
-import extraImage from '../../assets/UN-Photo.png';
-// Change to your actual image name
+
+import img1 from '../../assets/img1.png';
+import img2 from '../../assets/img2.png';
+import img3 from '../../assets/img3.png';
+import img4 from '../../assets/img4.png';
+import img5 from '../../assets/img5.png';
+
+const images = [img1, img2, img3, img4, img5];
 
 function UNPhotoImage() {
+  const [index, setIndex] = useState(0);
+
+  const handleNext = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
   return (
-    <div className="unphoto-image-container">
-      <img src={extraImage} alt="UN Additional Visual" className="unphoto-extra-img" />
+    <div className="slider-container">
+      <img src={images[index]} alt={`Slide ${index + 1}`} className="slider-image" />
+      <button className="slider-button" onClick={handleNext}>
+        &gt;
+      </button>
     </div>
   );
 }
 
 export default UNPhotoImage;
+
